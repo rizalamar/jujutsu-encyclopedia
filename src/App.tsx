bfb2c8ff11099f54397e7e15bc905414154ada80
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-04 23:22:29 +0700

    test data in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import type { JikanResponse, Technique } from "./@types";
+import { techniquesData } from "./data/techniques";
+import { getJJKCharacters } from "./services/character.service";
+
 function App() {
+	const [characters, setCharacters] = useState<JikanResponse | null>(null);
+	const [techniques] = useState<Technique[]>(techniquesData);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getJJKCharacters();
+				setCharacters(data);
+				console.log("API Characters: ", data.data);
+				console.log("Local Technique: ", techniques);
+
+				const gojo = data.data.find((c) => c.character.name.toLowerCase().includes("gojo"));
+				if (gojo) {
+					const gojoTechnique = techniques.filter((t) => t.character_id === 1);
+					console.log(`Techniques for ${gojo.character.name}:`, gojoTechnique);
+				}
+			} catch (error) {
+				console.error("Failed to fetch data: ", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [techniques]);
+
 	return (
-		<div className="">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		<div className="p-8">
+			<h1 className="text-3xl font-bold mb-4">Jujutsu kaisen Intelligence System</h1>
+
+			{loading ? (
+				<p>Loading Cursed Energy...</p>
+			) : (
+				<div className="grid gap-4">
+					<p className="text-green-500">{characters.data.length} Characters</p>
+					<p className="text-blue-500">{techniques.length} Local Technique</p>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/data/techniques.ts
+++ src/data/techniques.ts
@@ -1,23 +1,23 @@
-[
+export const techniquesData = [
 	{
 		"id": 1,
 		"character_id": 1,
 		"name": "Limitless: Red",
 		"description": "A convergence of infinity that creates a powerful repulsive force.",
-		"type": "Cursed Technique"
+		"type": "Cursed Technique",
 	},
 	{
 		"id": 2,
 		"character_id": 1,
 		"name": "Limitless: Blue",
 		"description": "An amplification of infinity that creates a powerful attactive force.",
-		"type": "Cursed Technique"
+		"type": "Cursed Technique",
 	},
 	{
 		"id": 2,
 		"character_id": 1,
 		"name": "Hollow Purple",
 		"description": "The combination of Red and Blue, creating a singularity that erases everything in its path",
-		"type": "Secret Technique"
-	}
-]
+		"type": "Secret Technique",
+	},
+];
